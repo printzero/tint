4d53756192cccbc375578e6b7917810b1652e236
codekid <ckrackerx15@gmail.com>
2019-03-05 01:27:29 +0530

    add example of palette

---
+++ examples/palette.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"tint"
+)
+
+func main() {
+	t := tint.Init()
+	// palette example
+	fmt.Println(t.Palette(
+		t.With("Ashish Shekar", tint.Black, tint.BgWhite),
+		t.With("is an", tint.Normal),
+		t.With("awesome", tint.Cyan),
+		t.With("guy!", tint.Normal),
+	))
+}

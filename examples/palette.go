d07e35b0f01975c0c082b2d0681802897e02a64b
codekid <ckrackerx15@gmail.com>
2019-03-10 22:47:46 +0530

    change example of pallete

--- examples/palette.go
+++ examples/palette.go
@@ -9,7 +9,7 @@ func main() {
 	t := tint.Init()
 	// palette example
 	fmt.Println(t.Palette(
-		t.With("Ashish Shekar", tint.Black, tint.BgWhite),
+		t.With("Ashish Shekar", tint.Magenta),
 		t.With("is an", tint.Normal),
 		t.With("awesome", tint.Cyan),
 		t.With("guy!", tint.Normal),
